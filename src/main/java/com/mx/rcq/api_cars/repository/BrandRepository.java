af24758d37d789899c5e19f79c1f98d2917f892b
Rogelio Cohen <rcohen@minsait.com>
2025-03-13 00:36:21 -0600

    Fix: Se agrega anotación @Repository a repositorios.

--- src/main/java/com/mx/rcq/api_cars/repository/BrandRepository.java
+++ src/main/java/com/mx/rcq/api_cars/repository/BrandRepository.java
@@ -3,10 +3,11 @@ package com.mx.rcq.api_cars.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.mx.rcq.api_cars.models.Brand;
 
-
+@Repository
 public interface BrandRepository extends JpaRepository<Brand, Long> {
     Optional<Brand> findByName(String name);
 }
--- src/main/java/com/mx/rcq/api_cars/repository/ModelRepository.java
+++ src/main/java/com/mx/rcq/api_cars/repository/ModelRepository.java
@@ -1,10 +1,11 @@
 package com.mx.rcq.api_cars.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.mx.rcq.api_cars.models.Model;
 
 import java.util.List;
-
+@Repository
 public interface ModelRepository extends JpaRepository<Model, Long> {
     List<Model> findByBrandId(Long brandId);
     List<Model> findByAveragePriceGreaterThan(int price);
